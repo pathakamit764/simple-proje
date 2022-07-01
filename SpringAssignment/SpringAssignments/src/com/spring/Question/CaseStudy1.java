149e687cb29c18fe678599c090d266a523199601
pathakamit764 <102358187+pathakamit764@users.noreply.github.com>
2022-07-01 11:36:33 +0530

    Add files via upload

---
+++ SpringAssignment/SpringAssignments/src/com/spring/Question/CaseStudy1.java
@@ -0,0 +1,32 @@
+package com.spring.Question;
+
+import org.springframework.context.event.ContextClosedEvent;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class CaseStudy1 {
+public static void main(String args[]) {
+	
+ClassPathXmlApplicationContext context=new ClassPathXmlApplicationContext("appllicationContext.xml");
+
+Question ques=(Question) context.getBean("questions");
+System.out.println("list");
+System.out.println(ques.getQuestionId());
+System.out.println(ques.getQuestion());
+System.out.println(ques.getAnswers());
+System.out.println("--------------------------------------------------------------------------");
+QuestionSet ques1=(QuestionSet) context.getBean("questions1");
+System.out.println("Set");
+System.out.println(ques1.getQuestionId1());
+System.out.println(ques1.getQuestion1());
+System.out.println(ques1.getAnswers1());
+System.out.println("--------------------------------------------------------------------------");
+QuestionMap ques2=(QuestionMap) context.getBean("questions2");
+System.out.println("map");
+System.out.println(ques2.getQuestionId2());
+System.out.println(ques2.getQuestion2());
+System.out.println(ques2.getAnswers2());
+System.out.println("--------------------------------------------------------------------------");
+
+context.close();
+}
+}
